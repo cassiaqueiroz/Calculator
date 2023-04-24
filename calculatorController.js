ce5672f7104f1f5a6acf85f292be456c5fa613ab
Cássia Queiroz <105453035+cassiaqueiroz@users.noreply.github.com>
2023-04-24 13:21:54 -0300

    Update calculatorController.js

--- calculatorController.js
+++ calculatorController.js
@@ -13,7 +13,7 @@
     Sub : function(component, event, helper) {
         var a = component.get("v.num1");
         var b = component.get("v.num2");
-            var total = parseFloat(a) - parseFloat(b);
+        var total = parseFloat(a) - parseFloat(b);
         component.set("v.total",total.toFixed(2));
         component.set("v.isAdd",false);
         component.set("v.isSub",true);
@@ -35,21 +35,27 @@
     Divi : function(component, event, helper) {
         var a = component.get("v.num1");
         var b = component.get("v.num2");
-         var total = parseFloat(a) / parseFloat(b);
-        component.set("v.total",total.toFixed(2));
-        component.set("v.isAdd",false);
-        component.set("v.isSub",false);
-        component.set("v.isMul",false);
-        component.set("v.isDiv",true);
-    },
-
-    
-    Clear : function(component, event, helper) {
-        component.set("v.total",0);
-        component.set("v.num1",0);
-        component.set("v.num2",0);
-        component.set("v.isAdd",false);
-        component.set("v.isSub",false);
-        component.set("v.isMul",false);
-    }
-})
+        if(b == 0){
+            alert('Não pode dividir números por zero!');
+        }
+        else{
+            var total = parseFloat(a) / parseFloat(b);
+            
+            component.set("v.total",total.toFixed(2));
+            component.set("v.isAdd",false);
+            component.set("v.isSub",false);
+            component.set("v.isMul",false);
+            component.set("v.isDiv",true);
+        }
+        },
+            
+            
+            Clear : function(component, event, helper) {
+                component.set("v.total",null);
+                component.set("v.num1",null);
+                component.set("v.num2",null);
+                component.set("v.isAdd",null);
+                component.set("v.isSub",null);
+                component.set("v.isMul",null);
+            }
+    })
